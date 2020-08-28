37a3694531a9df65a5045a5ab5700e9a8c8e8780
Nishant kumar  Behl <nrbehl@gmail.com>
2020-08-28 20:45:28 +0530

    added dao and entity

--- src/main/java/com/springboot/employee/EmployeeApp/Entity/EmployeeEntity.java
+++ src/main/java/com/springboot/employee/EmployeeApp/Entity/EmployeeEntity.java
@@ -1,2 +1,65 @@
-package com.springboot.employee.EmployeeApp.Entity;public class EmployeeEntity {
+package com.springboot.employee.EmployeeApp.Entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="Employee")
+public class EmployeeEntity {
+
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name="firstName")
+    private String firstName;
+    @Column(name="lastName")
+    private String lastName;
+    @Column(name="email")
+    private String email;
+
+    public EmployeeEntity() {
+    }
+
+    public EmployeeEntity(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "EmployeeEntity [id = " + id + ",FirstName = "+ firstName + ",LastName = " + lastName + ",Email = " + email +"]";
+    }
 }
