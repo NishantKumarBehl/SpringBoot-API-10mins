29220b42fa4856c3290ee2265f3a591a333f025f
Nishant kumar  Behl <nrbehl@gmail.com>
2020-08-30 02:00:45 +0530

    added Exception Handler code

--- src/main/java/com/springboot/employee/EmployeeApp/EmployeeRestExceptionClass/EmployeeRestExceptionHandler.java
+++ src/main/java/com/springboot/employee/EmployeeApp/EmployeeRestExceptionClass/EmployeeRestExceptionHandler.java
@@ -1,2 +1,23 @@
-package com.springboot.employee.EmployeeApp.EmployeeRestExceptionClass;public class EmployeeRestExceptionHandler {
+package com.springboot.employee.EmployeeApp.EmployeeRestExceptionClass;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class EmployeeRestExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<EmployeeExceptionClass> handleGenericExceptions(Exception exception){
+
+        EmployeeExceptionClass error = new EmployeeExceptionClass();
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(exception.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
 }
